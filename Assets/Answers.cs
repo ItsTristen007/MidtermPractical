38bb9003587d044da0b3ad0e4651133eabfbcf5f
ItsTristen007 <tristen.somwaru@ontariotechu.net>
2024-10-09 15:53:28 -0400

    Final

---
+++ Assets/Answers.cs
@@ -0,0 +1,24 @@
+/*
+ *Part 2
+ * Toon shaders are meant to make a an object look like it was for a cartoon
+ * This is done by instantly changing the diffuse and specular lighting instead of changing it gradually
+ * It uses a toon ramp to take a certain shade and transfer it to the object.
+ * I used it for the pills and the monsters because it makes the game feel more cartoon like and it fit the theme really well
+ *
+ *
+ * 
+ *
+ *Part 3
+ * 
+ * The colour correction is used to make the scene and enhance it by changing hue and colours 
+ * Here it is used to make the scene look like a hospital by changing the hue seen by the player into cool colours representing hospitals and their lights
+ *
+ *Part 4
+ * What was done:
+ * I made a transparent hologram type shader to make the jar see thru this gives the same effect as the original game but making it more modern.
+ * The shader takes advantage of the alpha channel which changes the alpha value for the material making it see thru.
+ * The code makes a slider to change the alpha value. 
+ *
+ *
+ * 
+ */
